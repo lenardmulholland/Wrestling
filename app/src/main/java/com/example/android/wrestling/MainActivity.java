7912cc57ba8ce7cd5f50208f6448b7c827c41158
Jennifer Mulholland <lenardmulholland@gmail.com>
2018-02-10 10:46:00 -0700

    Added save and restore functions for orientation changes and reduced number of calls to findViewById()

--- app/src/main/java/com/example/android/wrestling/MainActivity.java
+++ app/src/main/java/com/example/android/wrestling/MainActivity.java
@@ -5,33 +5,59 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.android.wrestling.R;
-
 public class MainActivity extends AppCompatActivity {
 
-    int scoreHome = 0;
-    int scoreVisitor = 0;
+    int scoreHome;
+    int scoreVisitor;
+    TextView scoreViewHome;
+    TextView scoreViewVisitor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            scoreHome = 0;
+            scoreVisitor = 0;
+            scoreViewHome = findViewById(R.id.home_score);
+            scoreViewVisitor = findViewById(R.id.visitor_score);
+    }
+
+    /**
+     * Saves scores when orientation changes
+     */
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("scoreHome", scoreHome);
+        savedInstanceState.putInt("scoreVisitor", scoreVisitor);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    /**
+     * Restores scores when orientation changes
+     */
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
         setContentView(R.layout.activity_main);
+        scoreHome = savedInstanceState.getInt("scoreHome");
+        scoreViewHome = findViewById(R.id.home_score);
+        scoreViewHome.setText(String.valueOf(scoreHome));
+        scoreVisitor = savedInstanceState.getInt("scoreVisitor");
+        scoreViewVisitor = findViewById(R.id.visitor_score);
+        scoreViewVisitor.setText(String.valueOf(scoreVisitor));
     }
 
     /**
      * Displays the given score for Home.
      */
     public void displayForHome(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.home_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewHome.setText(String.valueOf(score));
     }
 
     /**
      * Displays the given score for Visitor.
      */
     public void displayForVisitor(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.visitor_score);
-        scoreView.setText(String.valueOf(score));
+        scoreViewVisitor.setText(String.valueOf(score));
     }
 
     /**
